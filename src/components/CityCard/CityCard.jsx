1dbb807259663f97a35ea7075da740f7835669d1
Vincent Pasquiou <vincent.pasquiou@gmail.com>
2024-08-31 16:12:10 +0200

    accordeon  WIP, need to add data for each city

--- src/components/CityCard/CityCard.jsx
+++ src/components/CityCard/CityCard.jsx
@@ -1,25 +1,30 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
 export default function CityCard({ name }) {
-  useEffect(() => {
-    fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${name}&lang=fr&appid=${
-        import.meta.env.VITE_APP_ID
-      }`
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
-        }
-        return response.json();
-      })
-      .then((city) => {
-        console.info(city.name);
-      })
-      .catch((error) => {
-        console.error("Error fetching data", error);
-      });
-  }, [name]);
+  //   useEffect(() => {
+  //     fetch(
+  //       `https://api.openweathermap.org/data/2.5/weather?q=${name}&lang=fr&appid=${
+  //         import.meta.env.VITE_APP_ID
+  //       }`
+  //     )
+  //       .then((response) => {
+  //         if (!response.ok) {
+  //           throw new Error("Failed to fetch data");
+  //         }
+  //         return response.json();
+  //       })
+  //       .then((city) => {
+  //         console.info(city.name);
+  //       })
+  //       .catch((error) => {
+  //         console.error("Error fetching data", error);
+  //       });
+  //   }, [name]);
 
-  return <div> I am : {name}</div>;
+  return (
+    <details>
+      <summary>{name}</summary>
+      <p>Here is the content!</p>
+    </details>
+  );
 }
--- src/components/WrapperCards/WrapperCards.jsx
+++ src/components/WrapperCards/WrapperCards.jsx
@@ -2,13 +2,12 @@ import CityCard from "../CityCard/CityCard";
 import cities from "../../services/cityData";
 
 export default function WrapperCards() {
-
   return (
-    <div className="wrapperCards">
-      WrapperCards coucou
+    <section className="wrapperCards">
+      <h2>Liste des villes disponibles : </h2>
       {cities.map((city) => (
         <CityCard key={city.id} name={city.name} />
       ))}
-    </div>
+    </section>
   );
 }
